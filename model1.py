9c76cc1c208a87be87967587d69f2e822628af60
Rajiv (iMac) <rajivmanichand@gmail.com>
2020-04-26 10:55:27 +0200

    Write model1 as a class

--- model1.py
+++ model1.py
@@ -53,20 +53,29 @@ def articles_sentiment(articles, only_headline=False):
     return positivity, negativity
 
 
-def timespan_sentiment(articles, start_year, end_year, only_headlines=False):
-    """Returns the sentiment scores of a specific timespan within a set of articles"""
-    positive_scores, negative_scores = [], []
 
-    for year in range(start_year, end_year + 1):
-        for month in range(1, 13):
-            month_articles = art.months_articles(articles, month, year)
-            month_sentiment_pos, month_sentiment_neg = articles_sentiment(month_articles, only_headline=only_headlines)
 
-            positive_scores.append(month_sentiment_pos)
-            negative_scores.append(month_sentiment_neg)
+class Model1:
+    def __init__(self, articles):
+        self.articles = articles
+        pass
 
-    dates = pd.period_range(start="%d-1" % start_year, end="%d-12" % end_year, freq='M')
-    sentiment_scores = list(zip(positive_scores, negative_scores))
-    dataframe = pd.DataFrame(sentiment_scores, index=dates, columns=['Positive Scores', 'Negative Scores'])
+    def timespan_sentiment(self, start_year, end_year, only_headlines=False):
+        """Returns the sentiment scores of a specific timespan within a set of articles"""
+        articles = self.articles
+        positive_scores, negative_scores = [], []
 
-    return dataframe
+        for year in range(start_year, end_year + 1):
+            for month in range(1, 13):
+                month_articles = art.months_articles(articles, month, year)
+                month_sentiment_pos, month_sentiment_neg = articles_sentiment(month_articles,
+                                                                              only_headline=only_headlines)
+
+                positive_scores.append(month_sentiment_pos)
+                negative_scores.append(month_sentiment_neg)
+
+        dates = pd.period_range(start="%d-1" % start_year, end="%d-12" % end_year, freq='M')
+        sentiment_scores = list(zip(positive_scores, negative_scores))
+        dataframe = pd.DataFrame(sentiment_scores, index=dates, columns=['Positive Scores', 'Negative Scores'])
+
+        return dataframe
