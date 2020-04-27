1ca557f9f1733e27e6696669da9747749a9dad12
Rajiv (iMac) <rajivmanichand@gmail.com>
2020-04-27 11:51:40 +0200

    Implement model abstract class

---
+++ model.py
@@ -0,0 +1,50 @@
+from abc import ABC, abstractmethod
+import pandas as pd
+import articles as art
+
+
+class Model(ABC):
+    def __init__(self, articles):
+        self.articles = articles
+        pass
+
+    def timespan_sentiment(self, start_year, end_year):
+        """Returns the sentiment scores of a specific timespan within a set of articles"""
+        articles = self.articles
+        positive_scores, negative_scores = [], []
+
+        for year in range(start_year, end_year + 1):
+            for month in range(1, 13):
+                month_articles = art.months_articles(articles, month, year)
+                month_sentiment_pos, month_sentiment_neg = self.articles_sentiment(month_articles)
+
+                positive_scores.append(month_sentiment_pos)
+                negative_scores.append(month_sentiment_neg)
+
+        dates = pd.period_range(start="%d-1" % start_year, end="%d-12" % end_year, freq='M')
+        sentiment_scores = list(zip(positive_scores, negative_scores))
+        dataframe = pd.DataFrame(sentiment_scores, index=dates, columns=['Positive Scores', 'Negative Scores'])
+
+        return dataframe
+
+    def articles_sentiment(self, articles):
+        """Returns the positive and negative sentiment scores for a set of articles"""
+
+        if len(articles) == 0:
+            return 0, 0
+
+        positivity, negativity, neutrality = 0, 0, 0
+        for article in articles:
+            article_positivity, article_negativity, article_neutrality = self.article_sentiment(article)
+            positivity += article_positivity
+            negativity += article_negativity
+            neutrality += article_neutrality
+
+        positivity_indicator = positivity / len(articles)
+        negativity_indicator = negativity / len(articles)
+
+        return positivity_indicator, negativity_indicator
+
+    @abstractmethod
+    def article_sentiment(self, article):
+        pass
--- model1.py
+++ model1.py
@@ -1,6 +1,7 @@
 import csv
 import articles as art
 import pandas as pd
+from model import Model
 
 LM_NEGATIVE_WORDS, LM_POSITIVE_WORDS = [], []
 
@@ -15,67 +16,22 @@ with open('data/lm-sentiment-wordlist/lm_positive.csv') as f:
     LM_POSITIVE_WORDS = [word[0] for word in list(reader)]
 
 
-def tokens_sentiment(tokens):
-    """Returns the positive and negative sentiment scores of a list of tokens as a tuple"""
-    positive_tokens, negative_tokens = 0, 0
+class Model1(Model):
+    def article_sentiment(self, article):
+        """Returns the positive and negative sentiment scores of a list of tokens as a tuple"""
+        tokens = article.body_tokenized()
+        positive_tokens, negative_tokens, neutral_tokens = 0, 0, 0
 
-    for token in tokens:
-        token = token.upper()
-        if token in LM_POSITIVE_WORDS:
-            positive_tokens += 1
-        elif token in LM_NEGATIVE_WORDS:
-            negative_tokens += 1
+        for token in tokens:
+            token = token.upper()
+            if token in LM_POSITIVE_WORDS:
+                positive_tokens += 1
+            elif token in LM_NEGATIVE_WORDS:
+                negative_tokens += 1
+            else:
+                neutral_tokens += 1
 
-    positivity = positive_tokens / len(tokens)
-    negativity = negative_tokens / len(tokens)
-    return positivity, negativity
-
-
-def articles_sentiment(articles, only_headline=False):
-    """Returns the positive and negative sentiment scores for a set of articles"""
-
-    if len(articles) == 0:
-        return 0, 0
-
-    positivity, negativity = 0, 0
-    for article in articles:
-        if only_headline:
-            tokens = article.headline_tokenized()
-        else:
-            tokens = article.body_tokenized()
-        headline_positivity, headline_negativity = tokens_sentiment(tokens)
-        positivity += headline_positivity
-        negativity += headline_negativity
-
-    positivity = positivity / len(articles)
-    negativity = negativity / len(articles)
-
-    return positivity, negativity
-
-
-
-
-class Model1:
-    def __init__(self, articles):
-        self.articles = articles
-        pass
-
-    def timespan_sentiment(self, start_year, end_year, only_headlines=False):
-        """Returns the sentiment scores of a specific timespan within a set of articles"""
-        articles = self.articles
-        positive_scores, negative_scores = [], []
-
-        for year in range(start_year, end_year + 1):
-            for month in range(1, 13):
-                month_articles = art.months_articles(articles, month, year)
-                month_sentiment_pos, month_sentiment_neg = articles_sentiment(month_articles,
-                                                                              only_headline=only_headlines)
-
-                positive_scores.append(month_sentiment_pos)
-                negative_scores.append(month_sentiment_neg)
-
-        dates = pd.period_range(start="%d-1" % start_year, end="%d-12" % end_year, freq='M')
-        sentiment_scores = list(zip(positive_scores, negative_scores))
-        dataframe = pd.DataFrame(sentiment_scores, index=dates, columns=['Positive Scores', 'Negative Scores'])
-
-        return dataframe
+        positivity = positive_tokens / len(tokens)
+        negativity = negative_tokens / len(tokens)
+        neutrality = neutral_tokens / len(tokens)
+        return positivity, negativity, neutrality
