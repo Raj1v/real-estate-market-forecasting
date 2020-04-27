f16966c2d402170855cb966320d8b74ec393509a
Rajiv (iMac) <rajivmanichand@gmail.com>
2020-04-27 11:52:09 +0200

    update evaluation function

--- evaluation.py
+++ evaluation.py
@@ -17,15 +17,26 @@ class Evaluator:
 
         years = [article.year for article in articles]
 
-        sentiment = model.timespan_sentiment(min(years), max(years))
+        self.sentiment = model.timespan_sentiment(min(years), max(years))
 
+        self.print_evaluation()
+
+    def print_evaluation(self):
+        sentiment = self.sentiment
         negative_sentiment = sentiment[["Negative Scores"]]
         positive_sentiment = sentiment[["Positive Scores"]]
-
+        print("§" * 100)
+        print("POSITIVE SENTIMENT")
         self.var_results(positive_sentiment)
+        print("§" * 100)
+        print("§" * 100)
+        print("NEGATIVE SENTIMENT")
         self.var_results(negative_sentiment)
+        print("§" * 100)
 
     def var_results(self, sentiment_index):
         var = VectorAutoRegressor(housing_index=self.housing_index, sentiment_index=sentiment_index,
                                   housing_column="House price index")
+
+        var.forecast_period(var.test_data.index)
         print(var.fit.summary())
